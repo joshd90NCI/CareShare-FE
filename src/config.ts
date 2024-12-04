dda9e793145aa053c2528924621774b000c7a59a
jdancey-allview <jdancey@allview.ie>
2024-12-04 22:09:25 +0000

    small adjustment

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-const apiEndpoint = 'http://backend:8080';
+// const apiEndpoint = import.meta.env.VITE_API_ENDPOINT;
+const apiEndpoint = 'http://localhost:8080';
 const EXPIRY_TIME_IN_SECONDS = 5 * 60 * 60;
 
 export default { apiEndpoint, EXPIRY_TIME_IN_SECONDS };
