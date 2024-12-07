9ed07a33563bb1969d95457c7b93e2ea04e01606
jdancey-allview <jdancey@allview.ie>
2024-12-07 15:03:52 +0000

    Hardcoding just to check

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // const apiEndpoint = import.meta.env.VITE_API_ENDPOINT;
-const apiEndpoint = import.meta.env.VITE_API_ENDPOINT;
+const apiEndpoint = import.meta.env.VITE_API_ENDPOINT ?? 'http://care-share-backend:8080/api';
 const EXPIRY_TIME_IN_SECONDS = 5 * 60 * 60;
 
 export default { apiEndpoint, EXPIRY_TIME_IN_SECONDS };
