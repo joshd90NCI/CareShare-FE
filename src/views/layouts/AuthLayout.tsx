b1e2e20f127cb45ffb163ea9edc106c9ac457fb4
jdancey-allview <jdancey@allview.ie>
2024-12-08 16:37:18 +0000

    Auth layout changes, nginx config changes

--- src/views/layouts/AuthLayout.tsx
+++ src/views/layouts/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import logo from '../../assets/careshare-logo.png';
 
 import '../views.css';
 
@@ -6,6 +7,7 @@ const AuthLayout = () => {
   return (
     <section className="w-full h-dvh flex items-center justify-center flex-col main-bg relative">
       <h1 className="absolute top-10 text-4xl">Welcome to Care Share</h1>
+      <img src={logo} alt="Care Share Logo" className="w-48 mb-8" />
       <Outlet />
     </section>
   );
