c359f710dda8e2be5c0f73b5f4953d08ed602a4b
jdancey-allview <jdancey@allview.ie>
2024-12-08 16:30:31 +0000

    Swithcing around config files

--- src/components/PostInCollection.tsx
+++ src/components/PostInCollection.tsx
@@ -10,7 +10,7 @@ const PostInCollection: FC<Props> = ({ post }) => {
   const navigate = useNavigate();
   return (
     <div
-      className="bg-white my-3 p-3 flex items-center justify-between cursor-pointer hover:scale-105 transition-transform"
+      className="bg-white my-3 p-3 flex items-center justify-between cursor-pointer hover:scale-105 transition-transform text-start"
       onClick={() => navigate(`/post/${post.parentId ? post.parentId : post.id}`)}
       data-testid="post-in-collection"
     >
