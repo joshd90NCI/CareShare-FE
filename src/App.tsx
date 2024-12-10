3ea83eb08fb0cdf0198ecab09773e39142822962
jdancey-allview <jdancey@allview.ie>
2024-12-10 09:12:38 +0000

    Include lazy loading to improve performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 
-import AuthLayout from './views/layouts/AuthLayout.tsx';
-import MainLayout from './views/layouts/MainLayout.tsx';
-
-import LoginPage from './views/LoginPage.tsx';
-import RegisterPage from './views/RegisterPage.tsx';
-import PostCollection from './containers/PostCollection.tsx';
-import PostContainer from './containers/PostContainer.tsx';
 import { ModalOpenContextProvider } from './contexts/ModalContext.tsx';
 import { UserContextProvider } from './contexts/UserContext.tsx';
+import { AlertProvider } from './contexts/AlertContext.tsx';
 
 import './App.css';
-import SearchResults from './containers/SearchResults.tsx';
-import { AlertProvider } from './contexts/AlertContext.tsx';
-import ProfilePage from './views/ProfilePage.tsx';
-import AdminPage from './views/AdminPage.tsx';
-import LandingPage from './views/LandingPage.tsx';
+
+// Lazy load components
+const AuthLayout = lazy(() => import('./views/layouts/AuthLayout.tsx'));
+const MainLayout = lazy(() => import('./views/layouts/MainLayout.tsx'));
+const LoginPage = lazy(() => import('./views/LoginPage.tsx'));
+const RegisterPage = lazy(() => import('./views/RegisterPage.tsx'));
+const PostCollection = lazy(() => import('./containers/PostCollection.tsx'));
+const PostContainer = lazy(() => import('./containers/PostContainer.tsx'));
+const SearchResults = lazy(() => import('./containers/SearchResults.tsx'));
+const ProfilePage = lazy(() => import('./views/ProfilePage.tsx'));
+const AdminPage = lazy(() => import('./views/AdminPage.tsx'));
+const LandingPage = lazy(() => import('./views/LandingPage.tsx'));
 
 function App() {
   return (
@@ -23,24 +25,27 @@ function App() {
       <UserContextProvider>
         <ModalOpenContextProvider>
           <AlertProvider>
-            <Routes>
-              <Route>
-                <Route element={<AuthLayout />}>
-                  <Route path="register" element={<RegisterPage />} />
-                  <Route path="login" element={<LoginPage />} />
-                </Route>
-                <Route path="/" element={<MainLayout />}>
-                  <Route index element={<LandingPage />} />
-                  <Route path="search" element={<SearchResults />} />
-                  <Route path="posts/:type" element={<PostCollection />} />
-                  <Route path="post/:id" element={<PostContainer />} />
-                  <Route path="profile" element={<ProfilePage />} />
-                  <Route path="/admin">
-                    <Route index element={<AdminPage />} />
+            {/* Suspense fallback ensures smooth loading */}
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route>
+                  <Route element={<AuthLayout />}>
+                    <Route path="register" element={<RegisterPage />} />
+                    <Route path="login" element={<LoginPage />} />
+                  </Route>
+                  <Route path="/" element={<MainLayout />}>
+                    <Route index element={<LandingPage />} />
+                    <Route path="search" element={<SearchResults />} />
+                    <Route path="posts/:type" element={<PostCollection />} />
+                    <Route path="post/:id" element={<PostContainer />} />
+                    <Route path="profile" element={<ProfilePage />} />
+                    <Route path="/admin">
+                      <Route index element={<AdminPage />} />
+                    </Route>
                   </Route>
                 </Route>
-              </Route>
-            </Routes>
+              </Routes>
+            </Suspense>
           </AlertProvider>
         </ModalOpenContextProvider>
       </UserContextProvider>
